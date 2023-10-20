054685f80a1aa86de2abbcf1f2423140d0a90135
Adisak Rungruang <adisak01234@gmail.com>
2023-10-20 21:42:50 +0700

    refactor functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,10 +3,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import unittest
 import time
+from django.test import LiveServerTestCase
 
 
-class NewVisitorTest(unittest.TestCase):
-    # TODO: clear data before running
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -20,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_todo_list(self):
         # She checks out the homepage
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn("To-Do", self.browser.title)
@@ -52,6 +52,3 @@ class NewVisitorTest(unittest.TestCase):
         self.check_fow_row_in_list_table("1: Buy peacock features")
         self.check_fow_row_in_list_table("2: Use peacock features to make a fly")
 
-
-if __name__ == "__main__":
-    unittest.main()
