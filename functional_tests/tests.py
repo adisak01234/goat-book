579b4d6646d44fc06ff7cb9c76e167e65179cbd7
Adisak Rungruang <adisak01234@gmail.com>
2023-10-23 20:55:44 +0700

    add testing layout and using bootstrap

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,13 +4,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import unittest
 import time
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
 MAX_WAIT = 1
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -96,3 +96,29 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element(By.TAG_NAME, "body").text
         self.assertNotIn("Buy peacock features", page_text)
         self.assertNotIn("make a fly", page_text)
+
+    def test_layout_and_styling(self):
+        # user goes  to the home page
+        self.browser.get(self.live_server_url)
+
+        # her browser window is set to a very specific size
+        self.browser.set_window_size(1024, 768)
+
+        # she notices the input box is nicely centered
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertAlmostEquals(
+            inputbox.location["x"] + inputbox.size["width"] / 2,
+            512,
+            delta=10,
+        )
+
+        # she starts a new list and sees the input nicely centered there too
+        inputbox.send_keys("testing")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: testing")
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertAlmostEquals(
+            inputbox.location["x"] + inputbox.size["width"] / 2,
+            512,
+            delta=10,
+        )
