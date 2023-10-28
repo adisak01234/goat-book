bed0616453830aa34b26a72b57e731f311a67f94
Adisak Rungruang <adisak01234@gmail.com>
2023-10-28 19:36:10 +0700

    add functional tests for empty list

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -27,7 +27,17 @@ class FunctionalTest(StaticLiveServerTestCase):
                 rows = table.find_elements(By.TAG_NAME, "tr")
                 self.assertIn(row_text, [row.text for row in rows])
                 return
-            except (AssertionError, WebDriverException):
+            except (AssertionError, WebDriverException) as e:
                 if time.time() - start_time > MAX_WAIT:
-                    raise
+                    raise e
+                time.sleep(0.1)
+
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
                 time.sleep(0.1)
